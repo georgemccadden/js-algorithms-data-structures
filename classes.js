2100323d5cfb7fc423eea48e61316a206083d431
George McCadden III <thethirdone.g@gmail.com>
2019-06-04 23:34:18 -0700

    Added another layer of understanding to the class review

--- classes.js
+++ classes.js
@@ -1,9 +1,10 @@
 // A class is like a blueprint for creating objects with pre-defined properties and methods.
 
 class Student {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, grade) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.grade = grade;
   }
 }
 
@@ -11,5 +12,5 @@ class Student {
 
 // The keyword "class" creates a constant, which means I cannot redefine it.
 
-let studentOne = new Student("George", "McCadden III");
-let studentTwo = new Student("Miguel", "Monserate");
+let studentOne = new Student("George", "McCadden III", "junior");
+let studentTwo = new Student("Miguel", "Monserate", "senior");
