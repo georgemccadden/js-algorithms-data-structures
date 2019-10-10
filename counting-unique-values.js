8be883bb263a5d35af58373a420fa91cab0a8c7d
George McCadden III <thethirdone.g@gmail.com>
2019-10-09 18:19:29 -0700

    Practicing typing out my algorithm

---
+++ counting-unique-values.js
@@ -0,0 +1,15 @@
+// * PROBLEM DOMAIN *
+
+// Implement a function which accepts a sorted array and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+// * VISUAL *
+
+// countUniqueValues([1, 1, 1, 1, 1, 2]) // 2
+// countUniqueValues([]) // 0
+// countUniqueValues([-2, -1, -1, 0, 1]) // 4
+
+// * ALGORITHM *
+
+// - Create a function which accepts a sorted array.
+// -
+
