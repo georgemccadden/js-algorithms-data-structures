a6c30517e353fa3635df389dcc8baa7f97b582ab
George McCadden III <thethirdone.g@gmail.com>
2019-10-12 01:06:56 -0700

    I don't think this will run but I should save what I have

--- divide-and-conquer.js
+++ divide-and-conquer.js
@@ -2,11 +2,22 @@
 
 
 function search(array, val) {
-    let min = 0;
-    let max = array.length - 1;
+  let min = 0;
+  let max = array.length - 1;
 
-    while (min < max) {
-        let middle = Math.floor((min + max) / 2);
-        let currElement = array[middle];
+  while (min < max) {
+    let middle = Math.floor((min + max) / 2);
+    let currElement = array[middle];
+
+    if (arr[middle] < val) {
+      min = middle + 1;
     }
+    else if (array[middle] > val) {
+      max = middle - 1;
+    }
+    else {
+      return middle;
+    }
+  }
+  return -1;
 }
