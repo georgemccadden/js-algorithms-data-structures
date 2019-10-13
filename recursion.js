bf36d5d3d45ff40e51f1567578581d0e5206ee5a
George McCadden III <thethirdone.g@gmail.com>
2019-10-13 15:54:32 -0700

    That damn indentation again and an invokation of the function

--- recursion.js
+++ recursion.js
@@ -5,12 +5,14 @@
 // Example :
 
 function countDown(num) {
-    if (num <= 0) {
-        console.log("All done!");
-        return;
-    }
-    console.log(num);
+  if (num <= 0) {
+    console.log('All done!');
+    return;
+  }
+  console.log(num);
     
-    num--;
-    countDown(num);
+  num--;
+  countDown(num);
 }
+
+countDown(8);
