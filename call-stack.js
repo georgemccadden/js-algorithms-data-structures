ebe9d2e746b58368356f69f8d4525cef0a7e6926
George McCadden III <thethirdone.g@gmail.com>
2019-10-13 15:33:38 -0700

    Demonstrating knowledge of the call stack

---
+++ call-stack.js
@@ -0,0 +1,19 @@
+// * CALL STACK *
+
+// Any time a function is invoked it is placed (push) on the top of the call stack. When JavaScript sees the "return" keyword or when the function ends, the compiler will remove (pop).
+
+function takeShower() {
+    return "Showering!";
+}
+
+function eatBreakfast() {
+    let meal = cookFood();
+
+    return `Eating ${meal}`;
+}
+
+function cookFood() {
+    let items = ["Oatmeal", "Eggs", "Protein Shake"];
+
+    return items[Math.floor(Math.random() * items.length)];
+}
