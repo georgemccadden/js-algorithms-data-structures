433060d78d6d448ad32981a1551ecd7acea3a73e
George McCadden III <thethirdone.g@gmail.com>
2020-06-08 23:09:14 -0700

    Finding the index halfway between the parameters

--- binary-search.js
+++ binary-search.js
@@ -3,4 +3,10 @@ function binarySearch(target, nums) {
 
     let floorIndex = -1; // 'floorIndex' and 'ceilingIndex' are the parameters of the collection of nums
     let ceilingIndex = nums.length; // -1 means the parameter before the first number in the collection and nums.length is the parameter after the last number in the collection
+
+    while (floorIndex + 1 < ceilingIndex) { // If there isn't at least one index between our parameters then the number most not be present
+    // Find the index halfway between your parameters and round down so that you don't get a half index
+
+    const distance = ceilingIndex - floorIndex;
+    }
 }
