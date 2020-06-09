8208e74b371b6de80f32be990766f3c81e6ceda3
George McCadden III <thethirdone.g@gmail.com>
2020-06-09 00:47:26 -0700

    Take care of base case

--- merge-sort.js
+++ merge-sort.js
@@ -1,3 +1,7 @@
 function logMergeSort(arrayToSort) {
-     
+     // Base Case : Arrays with fewer than 2 elements are sorted
+     if (arrayToSort.length < 2) {
+         return arrayToSort;
+     }
+     // Step 1 : Divide the array in half (round down to avoid 'half index')
 }
