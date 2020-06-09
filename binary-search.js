5759114a22f472364376e7e5022c6f46677bde95
George McCadden III <thethirdone.g@gmail.com>
2020-06-08 22:45:59 -0700

    Giving some context to what I am doing

--- binary-search.js
+++ binary-search.js
@@ -1,3 +1,6 @@
 function binarySearch(target, nums) {
-    
+    // First you want to see if the target appears in nums collection
+
+    // 'floorIndex' and 'ceilingIndex' are the parameters of the collection of nums
+    // -1 means the parameter before the first number in the collection and nums.length is the parameter after the last number in the collection
 }
