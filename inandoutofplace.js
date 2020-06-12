be96d89cc95b2b476ff3eb063343405113d590fc
George McCadden III <thethirdone.g@gmail.com>
2020-06-11 23:55:28 -0700

    Explaining why there is no need to return anything

--- inandoutofplace.js
+++ inandoutofplace.js
@@ -1,3 +1,7 @@
 function  squareArrayInPlace(intArray) {
-    
+    intArray.forEach((int, index) => {
+        intArray[index] *= int;
+    });
+
+    // There's no need to return anything because you modified the intArray in-place
 }
