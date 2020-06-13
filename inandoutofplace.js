435909459a856fd56cbb6cd36f3f24850496ea87
George McCadden III <thethirdone.g@gmail.com>
2020-06-12 22:50:06 -0700

    This could be confusing

--- inandoutofplace.js
+++ inandoutofplace.js
@@ -9,3 +9,9 @@ function  squareArrayInPlace(intArray) {
 // Beware of how in-place algorithms affect your code outside of the function :
 
 const originalArray = [2, 3, 4, 5];
+
+squareArrayInPlace(originalArray);
+
+console.log('original array : ' + originalArray);
+
+// Logs : original array : 4, 9, 16, 25
