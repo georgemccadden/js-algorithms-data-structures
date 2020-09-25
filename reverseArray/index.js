ab6b5c6f4766dbba8a2c058c80fea5ec9e5b6c9c
George McCadden III <thethirdone.g@gmail.com>
2020-09-25 14:33:24 -0700

    Write a clear algorithm

--- reverseArray/index.js
+++ reverseArray/index.js
@@ -2,3 +2,14 @@
 
 // Reverse an array of integers.
 
+// arr = [6, 4, -15, 8, 3, 9, 1]
+// return = [1, 9, 3, 8, -15, 4, 6]
+
+
+// Algorithm :
+
+// - Create a variable to keep track of the first element in the array
+// - If within the problem constraints then continue
+// - Iterate through the array until you reach the middle
+// - Swap the first index with the last and continue inward in the array until end of iteration.
+// - Return the array.
