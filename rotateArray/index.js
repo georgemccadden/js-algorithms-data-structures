610d4a09a345cda0c00673d6501a913e7114b33a
George McCadden III <thethirdone.g@gmail.com>
2020-09-26 17:25:15 -0700

    This solution passed all tests

--- rotateArray/index.js
+++ rotateArray/index.js
@@ -24,3 +24,20 @@
 
 // CODE :
 
+function rotateArray(arr, steps) {
+    steps = steps % arr.length;
+
+    reverse(arr, 0, arr.length - 1);
+    reverse(arr, 0, steps - 1);
+    reverse(arr, steps, arr.length -1);
+};
+
+function reverse(arr, start, end) {
+    while (start < end) {
+        let temp = arr[0];
+        arr[end] = arr[start];
+        arr[start] = temp;
+        start++;
+        end--;
+    }
+};
