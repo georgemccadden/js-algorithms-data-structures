db43869a0fb7cf61c55f86cfe193171dcef05ed9
George McCadden III <thethirdone.g@gmail.com>
2020-10-01 11:12:24 -0700

    Writing out a clear algorithm

--- binarySearch/index.js
+++ binarySearch/index.js
@@ -9,3 +9,13 @@
 // Output - 4
 // Explanation - 9 exists in 'nums' and its index is 4.
 
+
+// Algorithm :
+
+// - Keep track of the parameters
+// - Find the middle of the array
+// - Check if the middle element is the target, if so return its index.
+// - If the middle element is greater than the target, change the parameters to the first element and the element before that middle element and then repeat that search until it is found.
+// - If the middle element is less than the target, change the parameters to the element after that middle element to the last element in the array and then repeat that search until it is found.
+// - If the element is not found, return -1.
+
