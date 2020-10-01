4098298e6157fbe784c8ec725641df75e22d3955
George McCadden III <thethirdone.g@gmail.com>
2020-10-01 14:13:24 -0700

    Giving an explanation

--- printLinkedList/index.js
+++ printLinkedList/index.js
@@ -2,3 +2,14 @@
 
 // Given a pointer to the head node of a linked list, print each node's 'data' element, one per line. If the head pointer is null (indicating the list is empty), there is nothing to print.
 
+
+// Example :
+
+// Input - 'n' = 2
+//         16
+//         13
+// Output - 
+//          16
+//          13
+// Explanation -
+// There are two elements in the linked list. They are represented as 16 -> 13 -> NULL. So, the printLinkedList function should print 16 and 13 each on a new line.
