0671e21b8cacfc1d4ca496e8b029e29d0d0b823f
George McCadden III <thethirdone.g@gmail.com>
2020-10-01 11:51:20 -0700

    Stating the problem domain

---
+++ printLinkedList/index.js
@@ -0,0 +1,4 @@
+// Problem Domain :
+
+// Given a pointer to the head node of a linked list, print each node's 'data' element, one per line. If the head pointer is null (indicating the list is empty), there is nothing to print.
+
