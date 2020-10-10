c91fdb2d56a5c9877d4accdace9a04559fbe7cfb
George McCadden III <thethirdone.g@gmail.com>
2020-10-10 15:58:16 -0700

    Now giving an example

--- insertNodeAtTail/index.js
+++ insertNodeAtTail/index.js
@@ -1,3 +1,4 @@
 // Problem Domain :
 
-// 
+// You are given the pointer to the head node of a linked list and an integer to add to the list. Create a new node with the given integer. Insert this node at the tail of the linked list and return the head node of the linked list formed after inserting this new node. The given head pointer may be null, meaning that the initial list is empty.
+
