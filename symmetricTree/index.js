5bc15f24a1778481aa8dbc581b149b30a55e003e
George McCadden III <thethirdone.g@gmail.com>
2020-10-11 15:27:55 -0700

    Giving a clear example of a symmetrical tree

--- symmetricTree/index.js
+++ symmetricTree/index.js
@@ -2,3 +2,15 @@
 
 // Given a binary tree, check whether it is a mirror of itself (i.e. symmetric around its center)
 
+
+// Example :
+
+// Input - [1, 2, 2, 3, 4, 4, 3]
+
+//      1
+//     / \
+//    2   2
+//   / \ / \
+//  3  4 4  3
+
+// Output - true
