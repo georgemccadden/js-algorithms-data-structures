0d8bccdbfe200f6383f99b21937213010229146d
George McCadden III <thethirdone.g@gmail.com>
2020-10-13 22:23:14 -0700

    Giving a clear example

--- MinStack/index.js
+++ MinStack/index.js
@@ -2,3 +2,12 @@
 
 // Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
+
+// Example :
+
+// Input - 
+//["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output -
+//[null,null,null,null,-3,null,0,-2]
